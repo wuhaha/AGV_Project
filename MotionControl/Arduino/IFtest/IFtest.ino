c6b2ba43319aff2a9e490e1b56bde3a8f1984548
Wuhaha <fengshuangg@gmail.com>
2017-09-20 18:52:25 -0700

    Added IR remote test

--- MotionControl/Arduino/IFtest/IFtest.ino
+++ MotionControl/Arduino/IFtest/IFtest.ino
@@ -1,1 +1,22 @@
+#include <IRremoteInt.h>
+#include <IRremote.h>
+
+int rcv = 13;
+
+IRrecv irrecv(rcv);
+
+decode_results results;
+
+void setup(){
+ Serial.begin(9600);
+ irrecv.enableIRIn();
+}
+
+void loop(){
+  if(irrecv.decode(&results)){
+  Serial.println(results.value, HEX);
+  irrecv.resume();
+  }
+}
+
 
