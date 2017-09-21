d4a493f0bff258248a44a5175852fe1a0e96aeb0
Wuhaha <fengshuangg@gmail.com>
2017-09-21 01:54:03 -0700

    dc motor take command from serial

--- MotionControl/Arduino/dc_motor/dc_motor.ino
+++ MotionControl/Arduino/dc_motor/dc_motor.ino
@@ -1,51 +1,105 @@
 //Motor A
-const int motorPin1  = 9;  // Pin 14 of L293
-const int motorPin2  = 10;  // Pin 10 of L293
+const int motorL1  = 9;  
+const int motorL2  = 10;  
 //Motor B
-const int motorPin3  = 6; // Pin  7 of L293
-const int motorPin4  = 5;  // Pin  2 of L293
+const int motorR1  = 6; 
+const int motorR2  = 5; 
 
-const int motorPinEn1 = 3;
-const int motorPinEn2 = 11;
+const int motorLS = 3;
+const int motorRS = 11;
 
-//This will run only one time.
+int Spd = 250;
+int Duration = 1000;
+
+
 void setup(){
-     //Set pins as outputs
-    pinMode(motorPin1, OUTPUT);
-    pinMode(motorPin2, OUTPUT);
-    pinMode(motorPin3, OUTPUT);
-    pinMode(motorPin4, OUTPUT);
-    pinMode(motorPinEn1, OUTPUT);
-    pinMode(motorPinEn2, OUTPUT);
 
-    analogWrite(motorPinEn1, 250);
-    analogWrite(motorPinEn2, 250);
-    //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
+    pinMode(motorL1, OUTPUT);
+    pinMode(motorL2, OUTPUT);
+    pinMode(motorR1, OUTPUT);
+    pinMode(motorR2, OUTPUT);
+    pinMode(motorLS, OUTPUT);
+    pinMode(motorRS, OUTPUT);
+
+    Serial.begin(9600);
     
-    //This code will turn Motor B clockwise for 2 sec.
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, HIGH);
-    digitalWrite(motorPin3, HIGH);
-    digitalWrite(motorPin4, LOW);
-    delay(5000); 
-    //This code will turn Motor B counter-clockwise for 2 sec.
-    digitalWrite(motorPin1, HIGH);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, HIGH);
-    delay(5000);    
-    
-    //And this code will stop motors
-    digitalWrite(motorPin1, LOW);
-    digitalWrite(motorPin2, LOW);
-    digitalWrite(motorPin3, LOW);
-    digitalWrite(motorPin4, LOW);
-  
+    Stop();
+
 }
+     
+   void Turn_Left(int degree){ 
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, HIGH);
+    digitalWrite(motorR1, HIGH);
+    digitalWrite(motorR2, LOW);
+    Speed(255);
+    delay(degree);
+    }
+    
+   void Turn_Right(int degree){
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);
+    Speed(255);
+    delay(degree);  
+   }  
+   void Forward(int spd, int t){
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
+    digitalWrite(motorR1, HIGH);
+    digitalWrite(motorR2, LOW);
+    Speed(spd);
+    delay(t); 
+    }
 
+    void Backward(int spd, int t){
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, HIGH);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);
+    Speed(spd);
+    delay(t); 
+    }
+   
+  void Stop(){
+    Speed(0);
+    
+    digitalWrite(motorL1, LOW);
+    digitalWrite(motorL2, LOW);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, LOW);
+    }
+  
+
+
+  void Speed(int spd){
+    analogWrite(motorLS, spd);
+    analogWrite(motorRS, spd);
+    }
 
 void loop(){
-  
+  if(Serial.available()){
+    char serialin;
+    serialin = Serial.read();
+
+    switch(serialin){
+    case 'F' :
+      Forward(Spd,Duration);
+      break;
+    case 'B' :
+      Backward(Spd,Duration);
+      break;
+    case 'L' :
+      Turn_Left(Duration);
+      break;
+    case 'R' :
+      Turn_Right(Duration);
+      break;
+    default:
+      Stop();
+      }
+    }
 
 }
 
