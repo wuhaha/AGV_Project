9cf70b0fba70eb58e69b9d6769379a348d431960
Wuhaha <fengshuangg@gmail.com>
2017-10-12 02:56:07 -0700

    added python serial cmd

--- MotionControl/Arduino/AGV_Motion_Control/examples/LibTest/LibTest.ino
+++ MotionControl/Arduino/AGV_Motion_Control/examples/LibTest/LibTest.ino
@@ -27,6 +27,8 @@ void setup() {
   Serial.begin(115200);
 
   digitalWrite(Condition_Flag, LOW);
+
+  runmode = MOTOR_IDLE;
   
 }
 
@@ -48,42 +50,50 @@ void STM_mode(enum run_modes runmode)
     case MOTOR_IDLE :
     {
       digitalWrite(Condition_Flag, LOW);
+      //Serial.println('i');
       break;  
     }
     case FORWARD : 
     {
       motormode.Go_Straight(motor_speed,motor_steps);
+      Serial.println('f');
       break;
     }  
     case BACKWARD : 
     {
       motormode.Go_Backward(motor_speed,motor_steps);
+      Serial.println('b');
       break;
     } 
     case LEFT_TURN : 
     {
       motormode.Turn_Left(motor_speed,motor_steps);
+            Serial.println('l');
       break;
     } 
     case RIGHT_TURN : 
     {
       motormode.Turn_Right(motor_speed,motor_steps);
+            Serial.println('r');
       break;
     } 
     default: break;
   }
-  //runmode = MOTOR_IDLE;
+  runmode = MOTOR_IDLE;
 }
 
 run_modes ParseSerial()
 {
-  char picmd[4];
+  int picmd[3]={0,0,0};
   unsigned int inbyte = 0;
   while(Serial.available())
-  {picmd[inbyte] = Serial.read();Serial.println(picmd[inbyte]);inbyte++;}
+  {
+    picmd[inbyte] = Serial.read();
+    inbyte++;
+  }
   motor_speed = picmd[1];
   motor_steps = picmd[2];
-  //Serial.println(inbyte);
+  
   return picmd[0];
   
 }
