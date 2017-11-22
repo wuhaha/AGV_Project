188c07d60f4559aa8eda024bbf1ddbea1896259c
Wuhaha <fengshuangg@gmail.com>
2017-11-22 00:09:43 -0800

    formatted

--- ImgProcessing/RPI/picap.py
+++ ImgProcessing/RPI/picap.py
@@ -3,15 +3,25 @@ from datetime import datetime
 import time
 import os
 
-fPath = '/home/pi/livacapture/' + datetime.now().strftime('%m_%d_%y') + "/"
+class cameractl:
 
-if not os.path.exists(fPath):
-	os.makedirs(fPath)
+	fPath = '/home/pi/livacapture/' + datetime.now().strftime('%m_%d_%y') + "/"
 
-pica = picamera.PiCamera()
+	def __init__(self):
+		pass
 
-pica.start_preview()
-time.sleep(3)
-tit = fPath + datetime.now().strftime('%m_%d_%Y_%H%M%S') + '.jpg'
-pica.capture(tit)
-pica.stop_preview()
+	def caminit():
+		if not os.path.exists(fPath):
+			os.makedirs(fPath)
+
+		pica = picamera.PiCamera()
+		pica.start_preview()
+	
+	def taking_picture():
+		time.sleep(3)
+		tit = fPath + datetime.now().strftime('%m_%d_%Y_%H%M%S') + '.jpg'
+		pica.capture(tit)
+		return tit
+
+	def camstop():
+		pica.stop_preview()
